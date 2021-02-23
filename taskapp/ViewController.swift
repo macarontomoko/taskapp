1cbfaade350616cd2bd48463a403e2d9e9b8d92e
tomoko.kuroda <egao190305@gmail.com>
2021-02-23 20:50:49 +0900

    検索のメソッドを作った。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -23,15 +23,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // 以降内容をアップデートするとリスト内は自動的に更新される。
     var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)  // ←追加
     
-    
-    // 以下、 自分で追加
-    
-    //検索結果配列
-    var searchResult = [String]()
-    
-    
-    // 以上、自分で追加
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -41,113 +32,110 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         // 以下、自分で追加
         
-        //タスクカテゴリーの定義の追加
-        var task.category = categoryTextField.text!
-        
         //デリゲート先を自分に設定する。
         SearchBar.delegate = self
         
-        // 検索します。
-        let searchResult = realm.objects(task.category)
-        
         //何も入力されていなくてもReturnキーを押せるようにする。
         SearchBar.enablesReturnKeyAutomatically = false
+        
+        //検索ボタン押下時の呼び出しメソッド
+        func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+            SearchBar.endEditing(true)
             
-            //検索ボタン押下時の呼び出しメソッド
-            func searchBarSearchButtonClicked(searchBar: UISearchBar) {
-                SearchBar.endEditing(true)
+            // 検索します。
+            let predicate = NSPredicate(format: "category = %@, '\(SearchBar.text)'")
+            SearchResult = realm.objects(Task.self).filter(predicate)
+            
+            // 検索結果の件数
+            
+            let count = SearchResult.count
+            
+            if(count == 0) {
+                //検索文字列が空の場合はすべてを表示する。
+                taskArray = realm.objects(Task.self)
                 
-                //検索結果配列を空にする。
-                searchResult.removeAll()
-                
-                if(SearchBar.text == "") {
-                    //検索文字列が空の場合はすべてを表示する。
-                    searchResult = task.category
-                } else {
-                    
-                    //検索文字列を含むデータを検索結果配列に追加する。
-                    for data in task.category {
-                        if data.containsString(SearchBar.text!) {
-                            searchResult.append(data)
-                        }
-                    }
-                }
+            } else {
+                taskArray = SearchResult
                 //テーブルを再読み込みする。
                 tableView.reloadData()
             }
-            // 以上、自分で追加
-            // データの数（＝セルの数）を返すメソッド
-            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                return taskArray.count  // ←修正する
-            }
-            
-            // 各セルの内容を返すメソッド
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                // 再利用可能な cell を得る
-                let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-                
-                // Cellに値を設定する.  --- ここから ---
-                let task = taskArray[indexPath.row]
-                cell.textLabel?.text = task.title
-                
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy-MM-dd HH:mm"
-                
-                let dateString:String = formatter.string(from: task.date)
-                cell.detailTextLabel?.text = dateString
-                
-                // --- ここまで追加 ---
-                
-                return cell
-            }
-            
-            // 各セルを選択した時に実行されるメソッド
-            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                performSegue(withIdentifier: "cellSegue",sender: nil)
-            }
-            
-            // セルが削除が可能なことを伝えるメソッド
-            func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
-                return .delete
-            }
-            
-            // Delete ボタンが押された時に呼ばれるメソッド
-            func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-                // --- ここから ---
-                if editingStyle == .delete {
-                    // データベースから削除する
-                    try! realm.write {
-                        self.realm.delete(self.taskArray[indexPath.row])
-                        tableView.deleteRows(at: [indexPath], with: .fade)
-                    }
-                } // --- ここまで追加 ---
-            }
-            
-            // segue で画面遷移する時に呼ばれる
-            override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-                let inputViewController:InputViewController = segue.destination as! InputViewController
-                
-                if segue.identifier == "cellSegue" {
-                    let indexPath = self.tableView.indexPathForSelectedRow
-                    inputViewController.task = taskArray[indexPath!.row]
-                } else {
-                    let task = Task()
-                    
-                    let allTasks = realm.objects(Task.self)
-                    if allTasks.count != 0 {
-                        task.id = allTasks.max(ofProperty: "id")! + 1
-                    }
-                    
-                    inputViewController.task = task
-                }
-            }
-            
-            
-            // 入力画面から戻ってきた時に TableView を更新させる
-            override func viewWillAppear(_ animated: Bool) {
-                super.viewWillAppear(animated)
-                tableView.reloadData()
-            }
-
+        }
     }
+}
+// 以上、自分で追加
+
+// データの数（＝セルの数）を返すメソッド
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return taskArray.count  // ←修正する
+}
+
+// 各セルの内容を返すメソッド
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // 再利用可能な cell を得る
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    
+    // Cellに値を設定する.  --- ここから ---
+    let task = taskArray[indexPath.row]
+    cell.textLabel?.text = task.title
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd HH:mm"
+    
+    let dateString:String = formatter.string(from: task.date)
+    cell.detailTextLabel?.text = dateString
+    
+    // --- ここまで追加 ---
+    
+    return cell
+}
+
+// 各セルを選択した時に実行されるメソッド
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    performSegue(withIdentifier: "cellSegue",sender: nil)
+}
+
+// セルが削除が可能なことを伝えるメソッド
+func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+    return .delete
+}
+
+// Delete ボタンが押された時に呼ばれるメソッド
+func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    // --- ここから ---
+    if editingStyle == .delete {
+        // データベースから削除する
+        try! realm.write {
+            self.realm.delete(self.taskArray[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    } // --- ここまで追加 ---
+}
+
+// segue で画面遷移する時に呼ばれる
+override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+    let inputViewController:InputViewController = segue.destination as! InputViewController
+    
+    if segue.identifier == "cellSegue" {
+        let indexPath = self.tableView.indexPathForSelectedRow
+        inputViewController.task = taskArray[indexPath!.row]
+    } else {
+        let task = Task()
+        
+        let allTasks = realm.objects(Task.self)
+        if allTasks.count != 0 {
+            task.id = allTasks.max(ofProperty: "id")! + 1
+        }
+        
+        inputViewController.task = task
+    }
+}
+
+
+// 入力画面から戻ってきた時に TableView を更新させる
+override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    tableView.reloadData()
+}
+
+}
 }
