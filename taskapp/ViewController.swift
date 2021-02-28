b21e046aecf0046ae4d31a1abdc973b9fefd85d3
tomoko.kuroda <egao190305@gmail.com>
2021-02-28 10:39:58 +0900

    再々提出。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -97,22 +97,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
         // --- ここから ---
         if editingStyle == .delete {
             // 削除するタスクを取得する
             let task = self.taskArray[indexPath.row]
-            
+
             // ローカル通知をキャンセルする
             let center = UNUserNotificationCenter.current()
             center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
-            
+
             // データベースから削除する
             try! realm.write {
                 self.realm.delete(task)
                 tableView.deleteRows(at: [indexPath], with: .fade)
             }
-            
+
             // 未通知のローカル通知一覧をログ出力
             center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
                 for request in requests {
@@ -122,14 +121,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 }
             }
         } // --- ここまで変更 ---
-        // --- ここから ---
-        if editingStyle == .delete {
-            // データベースから削除する
-            try! realm.write {
-                self.realm.delete(self.taskArray[indexPath.row])
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }
-        } // --- ここまで追加 ---
     }
     
     // segue で画面遷移する時に呼ばれる
